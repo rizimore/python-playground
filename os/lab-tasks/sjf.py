c9f411629255f618bbeaf728326e159178f5d297
rizimore <rizimore@gmail.com>
2021-03-09 00:12:04 +0500

    scheduler programming

--- os/lab-tasks/sjf.py
+++ os/lab-tasks/sjf.py
@@ -1,37 +1,12 @@
-"""
-First Come First Served
-
-@author Mubashir Rasool Razvi <sp19-bse-006@cuilahore.edu.pk>
-@date 03-08-2021
-
-Instructions:
-====
-
-1) Take input from user
-    - number of processes
-    - and burst time of each process
-
-2) We need to find out following information:
-    - arrival time
-    - waiting time
-    - average waiting time
-    - turn around time
-    - response time
-"""
-
 # init chart
 import matplotlib.pyplot as plt
 
-# Requirements
-arrival_times = []
 burst_times = []
+burst_times_unsorted = []
 completion_times = []
 waiting_times = []
 turnaround_times = []
-avg_waiting_time = None
 
-
-# Take input from user
 no_of_processes = int(input("Enter number of processes: "))
 
 if no_of_processes == 0:
@@ -39,12 +14,11 @@ if no_of_processes == 0:
     exit(1)
 
 for i in range(0, no_of_processes):
-    arrival_time = int(input(f"Enter arrival time of process {i+1}: "))
     burst_time = int(input(f"Enter burst time of process {i+1}: "))
-
-    arrival_times.append(arrival_time)
     burst_times.append(burst_time)
 
+burst_times_unsorted = burst_times.copy()
+burst_times.sort()
 
 # Calculate completion time of every process
 for i in range(0, no_of_processes):
@@ -56,21 +30,12 @@ for i in range(0, no_of_processes):
 
 # Calculate turn around time of every process
 for i in range(0, no_of_processes):
-    turnaround_times.append(completion_times[i] - arrival_times[i])
+    turnaround_times.append(completion_times[i] - 0)
 
 
 # Calculate waiting time of every process
 for i in range(0, no_of_processes):
     waiting_times.append(turnaround_times[i] - burst_times[i])
-
-
-# Calculate average time
-sum_of_waiting_times = 0
-count_of_waiting_times = len(waiting_times)
-for i in range(0, no_of_processes):
-    sum_of_waiting_times += waiting_times[i]
-avg_waiting_time = sum_of_waiting_times/count_of_waiting_times
-
 
 # configure chart
 fig, gnt = plt.subplots()
@@ -82,11 +47,13 @@ gnt.set_xlabel('BURST TIME')
 gnt.set_ylabel('P ID')
 
 y_tickers = []
+y_tickers_labels = []
 for i in range(0, no_of_processes):
     y_tickers.append(i+1)
+    y_tickers_labels.append(burst_times_unsorted.index(burst_times[i]) + 1)
 
 gnt.set_yticks(y_tickers)
-gnt.set_yticklabels(y_tickers)
+gnt.set_yticklabels(y_tickers_labels)
 
 for i in range(0, no_of_processes):
     if i == 0:
